1599097dda505ef1ee724280386cb2a19b987a38
Huy Dang <ddhuy137@yahoo.com>
2025-01-04 22:11:54 -0800

    updated core files (*.s & *.ld)

--- src/main.c
+++ src/main.c
@@ -8,6 +8,119 @@
 #include "main.h"
 
 
+/**
+  * @brief  Configures the System clock source, PLL Multiplier and Divider factors,
+  *         AHB/APBx prescalers and Flash settings
+  * @Note   This function should be called only once the RCC clock configuration
+  *         is reset to the default reset state (done in SystemInit() function).
+  * @param  None
+  * @retval None
+  */
+static void SetSysClock(void)
+{
+/******************************************************************************/
+/*            PLL (clocked by HSI) used as System clock source                */
+/******************************************************************************/
+	__IO uint32_t HSIStatus = 0;
+
+	/* Enable HSE */
+	RCC->CR |= ((uint32_t) RCC_CR_HSION);
+
+	/* Wait till HSE is ready and if Time out is reached exit */
+	do {
+		HSIStatus = RCC->CR & RCC_CR_HSIRDY;
+	} while (HSIStatus == 0);
+
+	if ((RCC->CR & RCC_CR_HSIRDY) != RESET) {
+		HSIStatus = (uint32_t) 0x01;
+	} else {
+		HSIStatus = (uint32_t) 0x00;
+	}
+
+	if (HSIStatus == (uint32_t) 0x01) {
+		/* Select regulator voltage output Scale 1 mode */
+		RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+		PWR->CR |= PWR_CR_VOS;
+
+		/* HCLK = SYSCLK / 1*/
+		RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+
+		/* PCLK2 = HCLK / 2*/
+		RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+
+		/* PCLK1 = HCLK / 4*/
+		RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+
+		/* Configure the main PLL */
+		RCC->PLLCFGR = RCC_PLLCFGR_PLLM | RCC_PLLCFGR_PLLN | RCC_PLLCFGR_PLLP
+				| RCC_PLLCFGR_PLLSRC_HSI | RCC_PLLCFGR_PLLQ;
+
+		/* Enable the main PLL */
+		RCC->CR |= RCC_CR_PLLON;
+
+		/* Wait till the main PLL is ready */
+		while ((RCC->CR & RCC_CR_PLLRDY) == 0) {
+		}
+
+		/* Enable the Over-drive to extend the clock frequency to 180 Mhz */
+		PWR->CR |= PWR_CR_ODEN;
+		while ((PWR->CSR & PWR_CSR_ODRDY) == 0) {
+		}
+		PWR->CR |= PWR_CR_ODSWEN;
+		while ((PWR->CSR & PWR_CSR_ODSWRDY) == 0) {
+		}
+
+		/* Configure Flash prefetch, Instruction cache, Data cache and wait state */
+		FLASH->ACR = FLASH_ACR_PRFTEN | FLASH_ACR_ICEN |FLASH_ACR_DCEN |FLASH_ACR_LATENCY_5WS;
+
+		/* Select the main PLL as system clock source */
+		RCC->CFGR &= (uint32_t) ((uint32_t) ~(RCC_CFGR_SW));
+		RCC->CFGR |= RCC_CFGR_SW_PLL;
+
+		/* Wait till the main PLL is used as system clock source */
+		while ((RCC->CFGR & (uint32_t) RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL) {
+		}
+
+	} else { /* If HSE fails to start-up, the application will have wrong clock
+	 configuration. User can add here some code to deal with this error */
+	}
+}
+
+
+/**
+  * @brief  Setup the micro-controller system
+  *         Initialize the Embedded Flash Interface, the PLL and update the
+  *         SystemFrequency variable.
+  * @param  None
+  * @retval None
+  */
+void SystemInit(void)
+{
+    /* Reset the RCC clock configuration to the default reset state ----------*/
+	/* Set HSION bit */
+	RCC->CR |= (uint32_t) 0x00000001;
+
+	/* Reset CFGR register */
+	RCC->CFGR = 0x00000000;
+
+	/* Reset HSEON, CSSON and PLLON bits */
+	RCC->CR &= (uint32_t) 0xFEF6FFFF;
+
+	/* Reset PLLCFGR register */
+	RCC->PLLCFGR = 0x24003010;
+
+	/* Reset HSEBYP bit */
+	RCC->CR &= (uint32_t) 0xFFFBFFFF;
+
+	/* Disable all interrupts */
+	RCC->CIR = 0x00000000;
+
+	/* Configure the System clock source, PLL Multiplier and Divider factors,
+	 AHB/APBx pre-scalers and Flash settings ---------------------------------*/
+	SetSysClock();
+}
+
+
 /****
  * Setup the User Button pins
  */
@@ -78,6 +191,8 @@ void init_sysclock(void)
 	// then set 1 wait-state and enable the pre-fetch buffer.
 	FLASH->ACR &= ~(FLASH_ACR_LATENCY | FLASH_ACR_PRFTEN);
 	FLASH->ACR |= (FLASH_ACR_LATENCY | FLASH_ACR_PRFTEN);
+	/* Reset CFGR register */
+	RCC->CFGR = 0x00000000;
 	// Configure the PLL to (HSI / 2) * 12 = 48MHz.
 	// Use a PLLMUL of 0xA for *12, and keep PLLSRC at 0
 	// to use (HSI / PREDIV) as the core source. HSI = 8MHz.
@@ -89,7 +204,7 @@ void init_sysclock(void)
 	// Select the PLL as the system clock source.
 	RCC->CFGR &= ~(RCC_CFGR_SW);
 	RCC->CFGR |= (RCC_CFGR_SW_PLL);
-	while (!(RCC->CFGR & RCC_CFGR_SWS_PLL)) {};
+	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL) {};
 	// Set the global clock speed variable.
 	core_clock_hz = 48000000;
 }
